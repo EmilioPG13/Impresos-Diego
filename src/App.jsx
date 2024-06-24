81b219cbeb9f24918c63ac0c16711b2bcec68007
Emilio <emilio.pg95@gmail.com>
2024-06-24 11:25:38 -0600

    refactor: Add header component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <DarkModeProvider>
-      <h1>Hello, world!</h1>
-    </DarkModeProvider>
+    // <DarkModeProvider>
+      <Header />
+    // </DarkModeProvider>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 function Header() {
-
+    return (
+        <header className="w-full relative">
+            <li className="outline outline-blue-600">
+                <a href="#" className="text-white text-2xl font-bold">Inicio</a>
+                <a href="#" className="text-white text-2xl font-bold">Nosotros</a>
+                <a href="#" className="text-white text-2xl font-bold">Servicios</a>
+                <a href="#" className="text-white text-2xl font-bold">Galeria</a>
+                <a href="#" className="text-white text-2xl font-bold">Contacto</a>
+                <a href="#" className="text-white text-2xl font-bold">Preguntas frecuentes</a>
+            </li>
+        </header>
+    )
 }
 
 export default Header;
