e99187beaff334c29fb5b7d14c94161d86d801e1
Emilio <emilio.pg95@gmail.com>
2024-06-24 09:07:06 -0600

    refactor: Update file imports and remove unused code

---
+++ src/components/DarkModeContext.jsx
@@ -0,0 +1,8 @@
+import { createContext, useState } from "react";
+import PropTypes from 'prop-types';
+
+export const DarkModeContext = createContext();
+
+export function DarkModeProvider() {
+ // Section not yet implemented
+}
