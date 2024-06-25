e87b9d440f2be59a0f79fe33a8c98466e7bfe8cc
Emilio <emilio.pg95@gmail.com>
2024-06-25 17:10:14 -0600

    🚧 refactor: Update header component and add social media icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,56 @@
 import logo from '../images/ID-logo.png';
+import tiktok from '../images/tiktok.png';
+import instagram from '../images/instagram.png';
+import facebook from '../images/facebook.png';
+import search from '../images/search-icon.png';
+
 
 function Header() {
     return (
-        <header className="w-full relative">
-            <div className='flex mb-1'>
-                {/* Adjusted image size for better visibility */}
-                <img src={logo} className="w-20 h-20 ml-3" alt="Logo" />
-                <ul className="outline outline-blue-600">
-                    {/* Consider using <Link> or <NavLink> from react-router-dom if you're using React Router */}
-                    <li><a href="#" className="text-white text-2xl font-bold">Inicio</a></li>
-                    <li><a href="#" className="text-white text-2xl font-bold">Nosotros</a></li>
-                    <li><a href="#" className="text-white text-2xl font-bold">Servicios</a></li>
-                    <li><a href="#" className="text-white text-2xl font-bold">Galeria</a></li>
-                    <li><a href="#" className="text-white text-2xl font-bold">Contacto</a></li>
-                    <li><a href="#" className="text-white text-2xl font-bold">Preguntas frecuentes</a></li>
-                </ul>
+        <header className="w-full relative border-t-8 border-blue-300">
+            {/* Parent container for left and right sides */}
+            <div className='flex justify-between items-center w-full'>
+                {/* Left side of the header */}
+                <div className='flex mb-1'>
+                    <a><img src={logo} className="w-20 h-20 m-3" alt="Logo" /></a>
+                    <ul className='display: flex m-9 ml-16 gap-10'>
+                        <li><a href="#" className="text-sky text-base">Inicio</a></li>
+                        <li><a href="#" className="text-sky text-base">Nosotros</a></li>
+                        <li><a href="#" className="text-sky text-base">Servicios</a></li>
+                        <li><a href="#" className="text-sky text-base">Galeria</a></li>
+                        <li><a href="#" className="text-sky text-base">Contacto</a></li>
+                        <li><a href="#" className="text-sky text-base">Preguntas frecuentes</a></li>
+                    </ul>
+                </div>
+
+                {/* Right side of the header */}
+                <section>
+                    <div className='flex justify-between'>
+                        <div className='flex justify-end space-x-4 m-5'>
+                            <a href='' className='text-lg'>
+                                <img src={tiktok} className="w-9 h-9" />
+                            </a>
+                            <a href='' className='text-lg'>
+                                <img src={instagram} className="w-9 h-9" />
+                            </a>
+                            <a href='' className='text-lg'>
+                                <img src={facebook} className="w-9 h-9" />
+                            </a>
+                        </div>
+
+                        <div className='flex flex-col space-y-0 m-2 mr-6 mb-5 border-r-2 border-gray-300 pr-6'>
+                            <p className='text-gray-500 text-sm'>Horario de atencion</p>
+                            <p className='text-sm'>9:00am a 6:00pm</p>
+                            <p className='text-sm'>222 248 8993</p>
+                        </div>
+
+                        <div>
+                            <a className='flex justify-end'>
+                                <img src={search} className="w-7 h-7 my-5 mr-9" />
+                            </a>
+                        </div>
+                    </div>
+                </section>
             </div>
         </header>
     );
