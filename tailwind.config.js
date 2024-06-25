9376cb815022d46da7d7d1fe6c639c429310b47d
Emilio <emilio.pg95@gmail.com>
2024-06-25 07:16:56 -0600

    ♻️ refactor: Update Tailwind CSS configuration to make it work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [ // Place your paths here
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
