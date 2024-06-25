1f31449fa670c5a495d38fd39339cf74767997e0
Emilio <emilio.pg95@gmail.com>
2024-06-25 07:16:39 -0600

    ♻️ refactor: Improve header component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import './styles/index.css'
 import Header from './components/Header'
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
+import logo from '../images/ID-logo.png';
+
 function Header() {
     return (
         <header className="w-full relative">
-            <li className="outline outline-blue-600">
-                <a href="#" className="text-white text-2xl font-bold">Inicio</a>
-                <a href="#" className="text-white text-2xl font-bold">Nosotros</a>
-                <a href="#" className="text-white text-2xl font-bold">Servicios</a>
-                <a href="#" className="text-white text-2xl font-bold">Galeria</a>
-                <a href="#" className="text-white text-2xl font-bold">Contacto</a>
-                <a href="#" className="text-white text-2xl font-bold">Preguntas frecuentes</a>
-            </li>
+            <div className='flex mb-1'>
+                {/* Adjusted image size for better visibility */}
+                <img src={logo} className="w-20 h-20 ml-3" alt="Logo" />
+                <ul className="outline outline-blue-600">
+                    {/* Consider using <Link> or <NavLink> from react-router-dom if you're using React Router */}
+                    <li><a href="#" className="text-white text-2xl font-bold">Inicio</a></li>
+                    <li><a href="#" className="text-white text-2xl font-bold">Nosotros</a></li>
+                    <li><a href="#" className="text-white text-2xl font-bold">Servicios</a></li>
+                    <li><a href="#" className="text-white text-2xl font-bold">Galeria</a></li>
+                    <li><a href="#" className="text-white text-2xl font-bold">Contacto</a></li>
+                    <li><a href="#" className="text-white text-2xl font-bold">Preguntas frecuentes</a></li>
+                </ul>
+            </div>
         </header>
-    )
+    );
 }
 
 export default Header;
