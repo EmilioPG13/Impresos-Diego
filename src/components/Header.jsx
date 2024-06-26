ea580f88dc4767bef7e5895a83f8a8eff5572505
Emilio <emilio.pg95@gmail.com>
2024-06-26 16:11:10 -0600

    🚧 refactor: Update Header component to include clickable logo, handle link clicks, and improve social media icons.
    Worked on the responsive design for 1024px

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,16 +30,13 @@ function Header() {
                 <div className='flex mb-1'>
                     <h1>
                         <button onClick={() => handleLinkClick('Inicio')} style={{ cursor: 'pointer' }}>
-                            <img src={logo} className="w-20 h-20 m-3" alt="Logo" />
+                            <img src={logo} className="w-20 h-20 m-3 lg:w-15 lg:-h-15" alt="Logo" />
                         </button>
                     </h1>
                     <nav>
-                        <ul className='flex m-9 ml-16 gap-10'>
+                        <ul className='flex m-9 ml-10 gap-10 lg:gap-5 lg:ml-3 xl:gap-10 xl:ml-10 mx-auto'>
                             <li>
                                 <a href="#" className={`text-sky text-base ${selectedLink === 'Inicio' ? 'border-b-2 border-blue-500 pb-1' : ''}`} onClick={() => handleLinkClick('Inicio')}>Inicio</a>
-                            </li>
-                            <li>
-                                <a href="#" className={`text-sky text-base ${selectedLink === 'Nosotros' ? 'border-b-2 border-blue-500 pb-1' : ''}`} onClick={() => handleLinkClick('Nosotros')}>Nosotros</a>
                             </li>
                             <li>
                                 <a href="#" className={`text-sky text-base ${selectedLink === 'Servicios' ? 'border-b-2 border-blue-500 pb-1' : ''}`} onClick={() => handleLinkClick('Servicios')}>Servicios</a>
@@ -51,7 +48,7 @@ function Header() {
                                 <a href="#" className={`text-sky text-base ${selectedLink === 'Contacto' ? 'border-b-2 border-blue-500 pb-1' : ''}`} onClick={() => handleLinkClick('Contacto')}>Contacto</a>
                             </li>
                             <li>
-                                <a href="#" className={`text-sky text-base ${selectedLink === 'Preguntas frecuentes' ? 'border-b-2 border-blue-500 pb-1' : ''}`} onClick={() => handleLinkClick('Preguntas frecuentes')}>Preguntas frecuentes</a>
+                                <a href="#" className={`text-sky text-base ${selectedLink === 'Preguntas frecuentes' ? 'border-b-2 border-blue-500 pb-1' : ''}`} onClick={() => handleLinkClick('Preguntas frecuentes')}>FAQ</a>
                             </li>
                         </ul>
                     </nav>
@@ -59,28 +56,28 @@ function Header() {
 
                 {/* Right side of the header */}
                 <section>
-                    <div className='flex justify-between'>
+                    <div className='flex justify-between mb-1'>
                         <div className='flex justify-end space-x-4 m-5'>
                             <a href='https://www.tiktok.com/@idmx.printhouse?_t=8nUzmUyoKur&_r=1' className='text-lg'>
-                                <img src={tiktok} className="w-9 h-9 hover:scale-105 hover:bg-fuchsia-400 rounded-full" />
+                                <img src={tiktok} className="w-7 h-7 hover:scale-105 hover:bg-fuchsia-400 rounded-full" />
                             </a>
                             <a href='https://www.instagram.com/impresosdiego?igsh=MXZpcnM1aTJoNTNpMQ==' className='text-lg'>
-                                <img src={instagram} className="w-9 h-9 hover:scale-105 hover:bg-rose-400 rounded-full" />
+                                <img src={instagram} className="w-7 h-7 hover:scale-105 hover:bg-rose-400 rounded-full" />
                             </a>
                             <a href='https://www.facebook.com/profile.php?id=61560933800832&mibextid=LQQJ4d' className='text-lg'>
-                                <img src={facebook} className="w-9 h-9 hover:scale-105 hover:bg-blue-400 rounded-full" />
+                                <img src={facebook} className="w-7 h-7 hover:scale-105 hover:bg-blue-400 rounded-full" />
                             </a>
                         </div>
 
-                        <div className='flex flex-col space-y-0 m-2 mr-6 mb-5 border-r-2 border-gray-300 pr-6'>
-                            <p className='text-gray-500 text-sm'>Horario de atencion</p>
-                            <p className='text-sm'>9:00am a 6:00pm</p>
-                            <p className='text-sm'>222 248 8993</p>
+                        <div className='flex flex-col space-y-0 m-2 mr-5 mb-5 border-r-2 border-gray-300 pr-4'>
+                            <p className='text-gray-500 text-xs'>Horario de atencion</p>
+                            <p className='text-xs'>9:00am a 6:00pm</p>
+                            <p className='text-xs'>222 248 8993</p>
                         </div>
 
                         <div className='flex'>
                             <button onClick={toggleSearchBar}>
-                                <img src={search} className="w-7 h-7 mb-3 mr-6 cursor-pointer" />
+                                <img src={search} className="w-7 h-7 mb-3 mr-6 p-0 cursor-pointer" />
                             </button>
 
                             {/* Render search bar conditionally with transition*/}
