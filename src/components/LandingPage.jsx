c9853c0cdd39641a127ddd4247cb0ab829fb0fdc
Emilio <emilio.pg95@gmail.com>
2024-07-03 10:03:06 -0600

    🚧 refactor: Update LandingPage component to include fade-in animations for text elements

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,8 +4,8 @@ function LandingPage() {
     return (
         <div className="h-screen w-full bg-cover bg-center" style={{ backgroundImage: `url(${frontpageImg})`}}> 
             <div className="flex flex-col items-center justify-center h-full w-full bg-black bg-opacity-50">
-                <p className="text-white text-lg">Más de 35 años de experiencia</p>
-                <h1 className="text-white text-8xl font-bold">Impresos Diego</h1>
+                <p className="text-white text-lg animate-fade-in-delay">Más de 35 años de experiencia</p>
+                <h1 className="text-white text-8xl font-bold animate__animated animate__fadeInDown">Impresos Diego</h1>
             </div>
         </div>
     )
