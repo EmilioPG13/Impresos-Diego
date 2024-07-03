5b40af72fa2cc4e935222b9454d3ca50df709a9b
Emilio <emilio.pg95@gmail.com>
2024-07-02 22:23:28 -0600

    refactor: Update App component to include a background color and wrap Header component in a div

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './styles/index.css'
 import Header from './components/Header'
 
 function App() {
-
   return (
-    // <DarkModeProvider>
-      <Header />
-    // </DarkModeProvider>
-  )
+    <div className="bg-stone-100 min-h-screen">
+      {/* <DarkModeProvider> */}
+        <Header />
+      {/* </DarkModeProvider> */}
+    </div>
+  );
 }
 
 export default App
