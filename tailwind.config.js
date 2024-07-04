16aef1072178ac5ee8cc16048a94a5a8b77256bd
Emilio <emilio.pg95@gmail.com>
2024-07-04 00:08:53 -0600

    refactor: Add Tailwind CSS plugin for gradient border top utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import plugin from 'tailwindcss/plugin';
+
+
 export default {
   content: [
     "./index.html",
@@ -17,5 +20,25 @@ export default {
       },
     },
   },
-  plugins: [],
+  
+  plugins: [
+    plugin(function({ addUtilities, theme }) { // Include theme in the function parameters
+      const newUtilities = {
+        '.gradient-border-top': {
+          position: 'relative',
+          '&::before': {
+            content: '""',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            right: '0',
+            height: '4px', // Adjust the thickness of the border
+            background: `linear-gradient(to right, ${theme('colors.cyan.500')}, ${theme('colors.blue.500')})`, // Correctly access theme colors
+            borderRadius: '2px', // Optional: if you want rounded corners
+          },
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+  ]
 }
