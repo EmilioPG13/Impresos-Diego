20d252cf8468d6b0c81c2b1053db17efd4e28329
Emilio <emilio.pg95@gmail.com>
2024-07-09 21:35:06 -0600

    🚧 feat: Add lazy loading for images in Gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,19 @@
+import { useEffect} from 'react';
+//Libraries
 import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'flowbite/dist/flowbite.css';
+
 
 function Gallery() {
+    useEffect(() => {
+        AOS.init({
+            duration: 2000,
+        });
+    }, []);
+
+
     return (
         <main>
             <header className="flex flex-col items-center justify-center text-center mb-12">
@@ -8,23 +21,23 @@ function Gallery() {
                 <h1 className='text-6xl my-4'>Nuestros trabajos</h1>
             </header>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 m-16">
-                <div style={{ animationDelay: '0.5s' }}>
-                    <img className="h-auto max-w-full rounded-lg animate__animated animate__fadeIn" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
+                <div data-aos='fade-in' data-aos-delay='250'>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
                 </div>
-                <div style={{ animationDelay: '1s' }}>
-                    <img className="h-auto max-w-full rounded-lg animate__animated animate__fadeIn" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+                <div data-aos='fade-in' data-aos-delay='500'>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
                 </div>
-                <div style={{ animationDelay: '1.5s' }}>
-                    <img className="h-auto max-w-full rounded-lg animate__animated animate__fadeIn" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
+                <div data-aos='fade-in' data-aos-delay='750'>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
                 </div>
-                <div style={{ animationDelay: '2s' }}>
-                    <img className="h-auto max-w-full rounded-lg animate__animated animate__fadeIn" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
+                <div data-aos='fade-in' data-aos-delay='1000'>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
                 </div>
-                <div style={{ animationDelay: '2.5s' }}>
-                    <img className="h-auto max-w-full rounded-lg animate__animated animate__fadeIn" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
+                <div data-aos='fade-in' data-aos-delay='1250'>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
                 </div>
-                <div style={{ animationDelay: '3s' }}>
-                    <img className="h-auto max-w-full rounded-lg animate__animated animate__fadeIn" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
+                <div data-aos='fade-in' data-aos-delay='2000'>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
                 </div>
             </div>
         </main>
