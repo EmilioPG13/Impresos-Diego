a2beb28348456cd73d61564a433ca647d2b6c9ef
Emilio <emilio.pg95@gmail.com>
2024-07-30 22:40:59 -0600

    ♻️ refactor: Update routing in both App.jsx (main) and index.jsx (Routes) add "Contacto" and "FAQ" routes. The desired routing is finally achieved.
    Routes without content 🚧:
    1. Servicios
    2. Galeria

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/index.css';
 import Header from './components/Header';
-import LandingPage from './components/LandingPage';
-import IntroPage from './components/IntroPage';
-import Gallery from './components/Gallery';
-import Acabados from './components/Acabados';
 import Footer from './components/Footer';
 import AppRoutes from './components/routes/index';
 
 function App() {
   return (
     <div className="bg-stone-50 min-h-screen">
-      {/* <DarkModeProvider> */}
       <Router>
         <Header />
-        <AppRoutes />
-        <LandingPage />
-        <IntroPage />
-        <Gallery />
-        <Acabados />
+        <Routes>
+          <Route path="/*" element={<AppRoutes />} />
+        </Routes>
         <Footer />
       </Router>
-      {/* </DarkModeProvider> */}
     </div>
   );
 }
--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -1,12 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
+import LandingPage from '../LandingPage';
+import IntroPage from '../IntroPage';
+import Gallery from '../Gallery';
+import Acabados from '../Acabados';
 import Contacto from '../routes/contacto';
 import FAQ from '../routes/faq';
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/faq' element={<FAQ />} />
-            <Route path='/contacto' element={<Contacto />} />
+            <Route path="/" element={
+                <>
+                    <LandingPage />
+                    <IntroPage />
+                    <Gallery />
+                    <Acabados />
+                </>
+            } />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/contacto" element={<Contacto />} />
         </Routes>
     );
 };
