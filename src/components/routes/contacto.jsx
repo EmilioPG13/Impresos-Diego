43762889eac3551b8be4191f96b3e4e9125fa12a
Emilio <emilio.pg95@gmail.com>
2024-08-25 12:54:35 -0600

    refactor: Update IntroPage content and layout

--- src/components/routes/contacto.jsx
+++ src/components/routes/contacto.jsx
@@ -1,71 +1,76 @@
 function Contacto() {
     return (
         <>
-            <div className="m-12">
-                <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-                    Cotiza Tu proyecto de impresion
-                </h1>
-                <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-                    Si estás buscando imprimir revistas o catálogos para tu negocio, institución o proyecto, estás en el lugar correcto. Solo deja tus datos aquí y te enviaremos un presupuesto en 72 horas. <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Ten en cuenta que solo nos encargamos de la impresión, no del diseño.</span>
-                </p>
-                <br/>
-                <p className="text-sm font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-                    Ingresa tu informacion y nosotros nos pondremos en contacto contigo en menos de 24 horas.
-                </p>
-            </div>
+            <section className="bg-white dark:bg-gray-900">
+                <div className="p-12 pb-0">
+                    <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+                        Cotiza Tu proyecto de impresión
+                    </h1>
+                    <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+                        Si estás buscando imprimir revistas o catálogos para tu negocio, institución o proyecto, estás en el lugar correcto. Solo deja tus datos aquí y te enviaremos un presupuesto en 72 horas.
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-sky-400 via-blue-500 to-emerald-600">
+                            Ten en cuenta que solo nos encargamos de la impresión, no del diseño. Cuantos más datos y especificaciones proporciones, más acertado será el precio aproximado para tus requerimientos.
+                        </span>
+                    </p>
+                    <br />
+                    <p className="text-sm font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+                        Ingresa tu información y nosotros nos pondremos en contacto contigo en menos de 24 horas.
+                    </p>
+                </div>
 
-            <form className="m-12">
-                <div className="grid gap-6 mb-6 md:grid-cols-2">
-                    <div>
-                        <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
-                        <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Juan" required />
-                    </div>
-                    <div>
-                        <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellidos</label>
-                        <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Perez" required />
-                    </div>
-                    <div>
-                        <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Numero de telefono</label>
-                        <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="(xxx) xxx - xxxx" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
-                    </div>
-                    <div>
-                        <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">¿Perteneces a alguna empresa?</label>
-                        <input type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre de la Empresa" required />
-                    </div>
+                <form className="m-12">
+                    <div className="grid gap-6 mb-6 md:grid-cols-2">
+                        <div>
+                            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Juan" required />
+                        </div>
+                        <div>
+                            <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellidos</label>
+                            <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Perez" required />
+                        </div>
+                        <div>
+                            <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Numero de telefono</label>
+                            <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="(xxx) xxx - xxxx" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+                        </div>
+                        <div>
+                            <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">¿Perteneces a alguna empresa?</label>
+                            <input type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre de la Empresa" required />
+                        </div>
 
-                    <div className="max-w-full">
-                        <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Trabajo a solicitar</label>
-                        <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                            <option selected>Elige una opción</option>
-                            <option value="US">Revista/Etiquetas</option>
-                            <option value="CA">Opción 1</option>
-                            <option value="FR">Opción 2</option>
-                            <option value="DE">Opción 3</option>
-                        </select>
+                        <div className="max-w-full">
+                            <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Trabajo a solicitar</label>
+                            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                                <option selected>Elige una opción</option>
+                                <option value="US">Revista/Etiquetas</option>
+                                <option value="CA">Opción 1</option>
+                                <option value="FR">Opción 2</option>
+                                <option value="DE">Opción 3</option>
+                            </select>
+                        </div>
+                        <div className="max-w-full">
+                            <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad</label>
+                            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                                <option selected>Elige una opción</option>
+                                <option value="US">300</option>
+                                <option value="CA">Opción 1</option>
+                                <option value="FR">Opción 2</option>
+                                <option value="DE">Opción 3</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="max-w-full">
-                        <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad</label>
-                        <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                            <option selected>Elige una opción</option>
-                            <option value="US">300</option>
-                            <option value="CA">Opción 1</option>
-                            <option value="FR">Opción 2</option>
-                            <option value="DE">Opción 3</option>
-                        </select>
+                    <div className="mb-6">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electronico</label>
+                        <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
                     </div>
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electronico</label>
-                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
-                </div>
-                <div className="flex items-start mb-6">
-                    <div className="flex items-center h-5">
-                        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+                    <div className="flex items-start mb-6">
+                        <div className="flex items-center h-5">
+                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+                        </div>
+                        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Estoy de acuerdo con <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">términos y condiciones</a>.</label>
                     </div>
-                    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Estoy de acuerdo con <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">términos y condiciones</a>.</label>
-                </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar</button>
-            </form>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar</button>
+                </form>
+            </section>
         </>
     );
 }
