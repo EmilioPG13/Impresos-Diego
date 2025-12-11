6a6fdc43b243d709473ff345c5f92f21a83806b4
Emilio <Emilio.pg95@gmail.com>
2025-12-11 13:32:16 -0600

    ⚰️ Remove unused files

--- src/components/DarkModeContext.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useState } from "react";
-import PropTypes from 'prop-types';
-
-export const DarkModeContext = createContext();
-
-export function DarkModeProvider() {
- // Section not yet implemented
-}
